e32cbb50849bacf54217f6c709b0bba5200f1b39
rbosko <prajmik@gmail.com>
2023-05-11 09:07:58 +0200

    oracle v1.1

--- oracle/web_app/routes.py
+++ oracle/web_app/routes.py
@@ -46,54 +46,26 @@ balancerQueryBody = """
 """
 
 
-def getWth():
+def getEthPrice():
   url1 = 'https://api.coingecko.com/api/v3/simple/price'
   url2 = 'https://api.binance.com/api/v3/ticker/price'
-  url3 = 'https://coinmarketcap.com/currencies/weth/'
-  params1 = {
-      'ids': 'weth',
-      'vs_currencies': 'usd'
-  }
-  params2 = {
-      'symbol': 'WETHUSD'
-  }
+
+  params1 = {'ids': 'weth', 'vs_currencies': 'usd'}
+  params2 = {'symbol': 'ETHUSDT'}
+
   response1 = requests.get(url1, params=params1)
   response2 = requests.get(url2, params=params2)
   weth_price = 0
 
   if response1.status_code == 200:
       data = response1.json()
-      # print(data)
-      weth_price += data['weth']['usd']
+      weth_price += float(data['weth']['usd'])
 
   if response1.status_code == 200:
       data = response2.json()
-      # print(data)
-      # weth_price += data['price']
+      weth_price += float(data['price'])
   
-  page = requests.get(url3)
-  soup = BeautifulSoup(page.content, "html.parser")
-  elements = soup.find_all("div", class_="priceValue")
-
-  # for entry in elements:
-  #   if entry.text[0] == '$':
-  #     # result = re.search('\$[0-9]+,[0-9]*\.[0-9]+<', elements)
-  #     weth_price += float(entry.text[1:].replace(',', ''))
-  #     break;
-
-  return weth_price
-
-# def getSingle():
-#   url3 = 'https://coinmarketcap.com/currencies/weth/'
-#   page = requests.get(url3)
-#   soup = BeautifulSoup(page.content, "html.parser")
-#   elements = soup.find_all("div", class_="priceValue")
-#   print(elements)
-#   for entry in elements:
-#     print(entry.text)
-#     if entry.text[0] == '$':
-#       # result = re.search('\$[0-9]+,[0-9]*\.[0-9]+<', elements)
-#       return float(entry.text[1:].replace(',', ''))
+  return weth_price / 2
 
 
 uniswapHistory = {}
@@ -123,12 +95,14 @@ def getBalancerTokens(url, body):
     return responseData
 
 def addUniswapHistoryEntry(data):
+    ethPrice = getEthPrice()
     for entry in data:
         if entry["id"] not in uniswapHistory:
           uniswapHistory[entry["id"]] = []
         if entry["token0"]["symbol"] not in uniformHistory:
           uniformHistory[entry["token0"]["symbol"]] = []
         valueEntry = {}
+        uniformEntry = {}
         valueEntry["dateTime"] = datetime.now().strftime("%H:%M:%S")
         valueEntry["token0name"] = entry["token0"]["symbol"]
         valueEntry["token0value"] = entry["token0Price"]
@@ -136,6 +110,10 @@ def addUniswapHistoryEntry(data):
         valueEntry["token1value"] = entry["token1Price"]
         uniswapHistory[entry["id"]].append(valueEntry)
 
+        if valueEntry["token1name"] == "WETH":
+          uniformEntry["dateTime"] = datetime.now().strftime("%H:%M:%S")
+          uniformEntry["price"] = float(valueEntry["token0value"]) * ethPrice
+          uniformHistory[entry["token0"]["symbol"]].append(uniformEntry)
 
 
 def addBalancerHistoryEntry(data):
@@ -166,7 +144,7 @@ def updateRates():
       pairs = getBalancerTokens(balanceApiURL, balancerQueryBody)
       addBalancerHistoryEntry(pairs)
 
-      time.sleep(3)
+      time.sleep(5)
       # print(history[pairs[0]["id"]][i]["token0value"])
       # print(balancerHistory[pairs[0]["symbol"]][i]["price"])
 
@@ -181,3 +159,12 @@ def getCurrencies():
           index = len(uniformHistory[entry]) - 1
           response.append({"symbol" : entry, "price" : uniformHistory[entry][index]["price"], "updated": uniformHistory[entry][index]["dateTime"]})
     return Response(json.dumps(response), status=200, mimetype='application/json')
+
+@app.route("/get-symbols")
+def getSymbols():
+    response = []
+    for entry in uniformHistory:
+        if len(uniformHistory[entry]) > 0:
+          index = len(uniformHistory[entry]) - 1
+          response.append(entry)
+    return Response(json.dumps({"length" : len(response), "symbols" : response}), status=200, mimetype='application/json')
