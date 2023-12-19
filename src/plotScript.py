2740bb11d950db443ea762d5f309c17331ef9fcb
rafalini <prajmik@gmail.com>
2023-12-19 11:29:13 +0100

    plots update

--- src/plotScript.py
+++ src/plotScript.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import glob, os
 
 directory_path = './node/log'  # Replace this with your directory path
@@ -14,19 +15,20 @@ dfs = []
 # Read each CSV file into a dataframe and store in the list
 for file in csv_files:
     df = pd.read_csv(file)
-    dfs.append(df)
+    dfs.append({"df":df, "name":file.split("_")[0]})
 
 # Plotting data from each dataframe on the same plot
 plt.figure(figsize=(10, 6))
 for df in dfs:
-    plt.plot(df['Timestamp'], df['USDvalue'], marker='o', label=df.columns[-1])
+    plt.plot(df["df"]['Timestamp'], df["df"]['USDvalue'], marker='o', label=df["name"])
 
 plt.xlabel('Timestamp')
 plt.ylabel('Price')
-plt.title('Price Data from CSV Files')
+plt.title('USD values in portfolio for each node')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
+plt.xticks(rotation=45)
 
 # Show the plot
 plt.show()
